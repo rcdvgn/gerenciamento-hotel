9978b82213e06d48751edd931f6497dfe6586929
rcdvgn <ricardorpvigliano@gmail.com>
2024-03-21 00:10:17 -0300

    commit inicial

--- app.py
+++ app.py
@@ -16,11 +16,19 @@ app.secret_key = 'SECRETE_KEY'
 # def make_session_permanent():
 #     session.permanent = True
 
+@app.route('/lost_connection')
+def lost_connection():
+    return render_template('lost_connection.html')
+
 @app.route('/')
 def index():
     if 'user' in session:
-        reservas = proxy.get_reservas(session['user']['reservas'], session['user_id']) 
-        return render_template('index.html', reservas=reservas)
+        try:
+            reservas = proxy.get_reservas(session['user']['reservas'], session['user_id']) 
+        except ConnectionRefusedError:
+            return redirect(url_for("lost_connection"))
+        
+        return render_template('index.html', reservas=reservas, user=session['user'])
     else:
         return redirect(url_for('login'))
 
@@ -31,8 +39,11 @@ def register():
         email = request.form['email']
         password = request.form['password']
 
-        status, response = proxy.register(name, email, password)
-        
+        try:
+            status, response = proxy.register(name, email, password)
+        except ConnectionRefusedError:
+            return redirect(url_for("lost_connection"))
+            
         if status == 1:
             session['user'] = response[0]
             session['user_id'] = response[1]
@@ -53,7 +64,10 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
-        status, response = proxy.login(email, password)
+        try:
+            status, response = proxy.login(email, password)
+        except ConnectionRefusedError:
+            return redirect(url_for("lost_connection"))
 
         if status == 1:
             session['user'] = response[0]
@@ -70,21 +84,28 @@ def login():
             return render_template('login.html')
         
 
-@app.route('/criar-reserva/<id>', methods=['POST', 'GET'])
-@app.route('/criar-reserva/<id>', methods=['POST', 'GET'])
+@app.route('/criar-reserva', methods=['POST', 'GET'])
+@app.route('/criar-reserva/<edit_id>', methods=['POST', 'GET'])
 def criar_reserva(edit_id=""):
     if request.method == 'POST':
         name = request.form['name']
         date = request.form['date']
         duration = request.form['duration']
+        hotel = request.form['hotel']
 
-        status, response = proxy.criar_reserva(name, date, duration, session['user_id'])
+        id = "" if not request.form['id'] else request.form['id']
+        room = "" if not request.form['room'] else request.form['room']
+
+        try:
+            status, response = proxy.criar_reserva(name, date, duration, id, room, hotel, session['user_id'])
+        except ConnectionRefusedError:
+            return redirect(url_for("lost_connection"))
 
         if status == 1:
-            updated_user = session['user']
+            updated_user = session['user'].copy()
             if not response in updated_user['reservas']:
                 updated_user['reservas'].append(response)
-                session['user'] = updated_user
+            session['user'] = updated_user
 
             return redirect(url_for('reserva', id=response))
         else:
@@ -93,17 +114,31 @@ def criar_reserva(edit_id=""):
     else:
         if 'user' in session:
             if edit_id != "":
-                status, response = proxy.reserva(edit_id, session['user_id'])
+                
+                try:
+                    status, response = proxy.reserva(edit_id, session['user_id'])
+                except ConnectionRefusedError:
+                    return redirect(url_for("lost_connection"))
 
                 if status == 1:
-                    updated_reserva = {
+                    existing_reserva = {
                         "quarto": response["quarto"],
                         "nome": response["nome"],
                         "data": response["data"],
                         "duracao": response["duracao"],
+                        "hotel": response["hotel"],
                         "id": response["id"],
                     }
-            return render_template('criar_reserva.html', updated_reserva=updated_reserva)
+                else:
+                    return f"<h2>{response}</h2>"
+            else:
+                existing_reserva = ""
+
+            try:
+                hoteis = proxy.get_all_hoteis()
+            except ConnectionRefusedError:
+                return redirect(url_for("lost_connection"))
+            return render_template('criar_reserva.html', existing_reserva=existing_reserva, hoteis=hoteis, user=session['user'])
             
         else:
             return redirect(url_for("login"))
@@ -115,33 +150,152 @@ def reserva(id):
        pass        
     else:
         if 'user' in session:
-            status, response = proxy.reserva(id, session['user_id'])
+            try:
+                status, response = proxy.reserva(id, session['user_id'])
+            except ConnectionRefusedError:
+                return redirect(url_for("lost_connection"))
 
             if status == 1:
-                return render_template('reserva.html', response=response)
+                return render_template('reserva.html', response=response, user=session['user'])
             else:
                 return f"<h1>{response}</h1>"
         else:
             return redirect(url_for("login"))
 
-# Rota personalizada
-@app.route('/consulta/<id>')
-def hello(id):
-    return render_template('consulta.html', id=id)
-
 @app.route('/excluir-reserva/<id>')
 def excluir_reserva(id):
     if 'user' in session:
-        status, response = proxy.excluir_reserva(id, session['user_id'])
+        try:
+            status, response = proxy.excluir_reserva(id, session['user_id'])
+        except ConnectionRefusedError:
+            return redirect(url_for("lost_connection"))
 
         if status == 1:
             session['user'] = response
-            return render_template('index.html')
+            return redirect(url_for('index'))
         else:
             return f"<h1>{response}</h1>"
     else:
         return redirect(url_for("login"))
+    
+@app.route('/consulta', methods=['POST'])
+def consulta():
+    if 'user' in session:
+        query = request.form["query"]
 
+        return redirect(url_for("reserva", id=query))
+    else:
+        return redirect(url_for("login"))
+
+@app.route('/admin', methods=['POST', 'GET'])
+def admin():
+    if request.method == 'POST':
+        pass
+    else:
+        if session['user']['admin'] == True:
+            try:
+                hoteis = proxy.get_all_hoteis()
+            except ConnectionRefusedError:
+                return redirect(url_for("lost_connection"))
+            
+            hoteis_info = []
+            for hotel in hoteis:
+                try:
+                    quartos = proxy.get_quartos(hotel)
+                except ConnectionRefusedError:
+                    return redirect(url_for("lost_connection"))
+                hoteis_info.append({"nome": hotel, "num_quartos": len(quartos.keys())})
+
+            return render_template('admin.html', user=session["user"], hoteis_info=hoteis_info)
+        else:
+            return "<h1>Acesso restrito</h1>"
+
+@app.route('/quartos/<hotel>', methods=['POST', 'GET'])
+def quartos(hotel):
+    if request.method == 'POST':
+        pass
+    else:
+        if session['user']['admin'] == True:            
+            try:
+                quartos = proxy.get_quartos(hotel)
+            except ConnectionRefusedError:
+                return redirect(url_for("lost_connection"))
+
+            return render_template('quartos.html', user=session["user"], hotel=hotel, quartos=quartos)
+        else:
+            return "<h1>Acesso restrito</h1>"
+
+@app.route('/adicionar-quarto/<hotel>', methods=['POST', 'GET'])
+def adicionar_quarto(hotel):
+    if request.method == 'POST':
+        num_quarto = request.form['num_quarto']
+
+        try:
+            status, response = proxy.adicionar_quarto(num_quarto, hotel)
+        except ConnectionRefusedError:
+            return redirect(url_for("lost_connection"))
+
+        if status == 0:
+            return f"<h1>{response}</h1>"
+        else:
+            return redirect(url_for('quartos', hotel=response))
+    else:
+        if session['user']['admin'] == True:
+            return render_template('adicionar_quarto.html', user=session["user"], hotel=hotel)
+        else:
+            return "<h1>Acesso restrito</h1>"
+
+@app.route('/adicionar-hotel/', methods=['POST', 'GET'])
+def adicionar_hotel():
+    if request.method == 'POST':
+        hotel_name = request.form['hotel_name']
+
+        try:
+            status, response = proxy.adicionar_hotel(hotel_name)
+        except ConnectionRefusedError:
+            return redirect(url_for("lost_connection"))
+
+        if status == 0:
+            return f"<h1>{response}</h1>"
+        else:
+            return redirect(url_for('quartos', hotel=response))
+    else:
+        if session['user']['admin'] == True:
+            return render_template('adicionar_hotel.html', user=session["user"])
+        else:
+            return "<h1>Acesso restrito</h1>"
+
+@app.route('/excluir-quarto/<hotel>/<quarto>', methods=['POST', 'GET'])
+def excluir_quarto(hotel, quarto):
+    if session['user']['admin'] == True:
+        try:
+            status, response = proxy.excluir_quarto(hotel, quarto, session['user_id'])
+        except ConnectionRefusedError:
+            return redirect(url_for("lost_connection"))
+
+        if status == 0:
+            return f"<h1>{response}</h1>"
+        else:
+            session['user'] = response
+            return redirect(url_for('admin'))
+    else:
+        return "<h1>Acesso restrito</h1>"
+
+@app.route('/excluir-hotel/<hotel>')
+def excluir_hotel(hotel):
+    if session['user']['admin'] == True:
+        try:
+            status, response = proxy.excluir_hotel(hotel, session['user_id'])
+        except ConnectionRefusedError:
+            return redirect(url_for("lost_connection"))
+
+        if status == 0:
+            return f"<h1>{response}</h1>"
+        else:
+            session['user'] = response
+            return redirect(url_for('admin'))
+    else:
+        return "<h1>Acesso restrito</h1>"
 
 @app.route('/logout')
 def logout():
--- servidor.py
+++ servidor.py
@@ -4,72 +4,111 @@ import uuid
 import random
 
 db = {
-    "reservas": {},
-    "quartos": [
-        101, 102, 103, 104, 105, 106, 
-        201, 202, 203, 204, 205, 206,
-        301, 302, 303, 304, 305, 306, 
-        401, 402, 403, 404, 405, 406
-    ],
     "usuarios": {
-        "1": {
+        "0": {
             "nome": "admin",
             "email": "admin@admin.com",
             "senha": "admin",
             "admin": True,
             "reservas": []
         }
+    },
+    "hoteis": {
+        "Ibis_Budget":{
+            "reservas": {},
+            "quartos": {
+                '101': "ocupado", '102': "livre", '103': "livre", '104': "ocupado", '105': "livre", '106': "livre", 
+                '201': "ocupado", '202': "ocupado", '203': "livre", '204': "ocupado", '205': "ocupado", '206': "livre"
+            }
+        },
+        "Ibis_Styles":{
+            "reservas": {},
+            "quartos": {
+                '101': "livre", '102': "livre", '103': "livre", '104': "ocupado", '105': "ocupado", '106': "ocupado", 
+                '201': "livre", '202': "ocupado", '203': "livre", '204': "livre", '205': "ocupado", '206': "livre"
+            }
+        },
+        "Ibis_Premium":{
+            "reservas": {},
+            "quartos": {
+                '101': "ocupado", '102': "ocupado", '103': "livre", '104': "livre", '105': "livre", '106': "livre", 
+                '201': "ocupado", '202': "livre", '203': "ocupado", '204': "livre", '205': "ocupado", '206': "livre"
+            }
+        }
     }
 }
 
-def get_reservas(reservas, user_id):
-    if not reservas and db['usuarios'][user_id]['admin']:
-        return list(db['reservas'].values())
-    else:
-        return [{'id': id, **db['reservas'][id]} for id in reservas if id in db['reservas']]
+def get_all_reservas():
+    all_reservas = {}
+    for hotel in db["hoteis"].values():
+        for hotel_reservas_id, hotel_reserva in hotel['reservas'].items():
+            all_reservas[hotel_reservas_id] = {'id': hotel_reservas_id, **hotel_reserva}
 
-def criar_reserva(name, date, duration, user_id):
-    id_reserva = str(uuid.uuid4()).replace('-', '')
+    return all_reservas
+
+def get_reservas(reservas, user_id):
+    all_reservas = get_all_reservas()
+
+    if db['usuarios'][user_id]['admin']:
+        return list(all_reservas.values())
+    else:
+        return [reserva for reserva in all_reservas.values() if reserva['id'] in reservas]
+    
+def quartos_livres(hotel):
+    return [num_quarto for num_quarto, status in db['hoteis'][hotel]['quartos'].items() if status != "ocupado"]
+
+def criar_reserva(name, date, duration, id, room, hotel, user_id):
+    id_reserva = str(uuid.uuid4()).replace('-', '') if not id else id
+
+    quarto = random.choice(quartos_livres(hotel)) if not room else room
+    
+    db['hoteis'][hotel]['quartos'][quarto] = "ocupado"
 
     reserva = {
-        "quarto": random.choice(db['quartos']),
+        "quarto": quarto,
         "nome": name,
         "data": date,
-        "duracao": duration
+        "duracao": duration,
+        "hotel": hotel
     }
     
-    db['reservas'][id_reserva] = reserva
-    db['usuarios'][user_id]['reservas'].append(id_reserva)
+    db['hoteis'][hotel]['reservas'][id_reserva] = reserva
+    db["usuarios"][user_id]['reservas'].append(id_reserva)
 
     return 1, id_reserva
 
 def reserva(id, user_id):
-    if id in db['reservas']:
+    all_reservas = get_all_reservas()
+    if id in all_reservas:
         if db['usuarios'][user_id]['admin'] or id in db['usuarios'][user_id]['reservas']:
-            return 1, {'id': id, **db['reservas'][id]}
+            return 1, all_reservas[id]
         else:
             return 0, "Acesso restrito"
     else:
         return 0, "Reserva inexistente"
 
 def excluir_reserva(id, user_id):
+    all_reservas = get_all_reservas()
+    
     if db['usuarios'][user_id]['admin'] == True:
-        if id in db['reservas']:
-            del db['reservas'][id]
+        if id in all_reservas:
+            del db['hoteis'][all_reservas[id]['hotel']]['reservas'][id]
             for user in db["usuarios"]:
                 if id in user['reservas']:
                     user["reservas"].remove(id)
+                    db['hoteis'][all_reservas[id]['hotel']]['quartos'][all_reservas[id]['quarto']] = "livre"
                     return 1, user
-                    break
+                    
+            
             return 0, "Reserva nao encontrada"
     else:
         if id in db["usuarios"][user_id]["reservas"]:
             db["usuarios"][user_id]["reservas"].remove(id)
-            del db['reservas'][id]
+            del db['hoteis'][all_reservas[id]['hotel']]['reservas'][id]
+            db['hoteis'][all_reservas[id]['hotel']]['quartos'][all_reservas[id]['quarto']] = "livre"
             return 1, db["usuarios"][user_id]
         return 0, "Reserva nao encontrada"
 
-    
 
 
 def register(name, email, password):
@@ -97,7 +136,58 @@ def login(email, password):
             return 1, [user, user_id]
         
     return 0, "Nenhum usuario encontrado"
+
+def get_quartos(hotel):
+    return db["hoteis"][hotel]["quartos"]
     
+def get_all_hoteis():
+    return list(db['hoteis'].keys())
+
+def adicionar_hotel(hotel_name):
+    if hotel_name in db['hoteis']:
+        return 0, "Hotel ja existente"
+    else:
+        new_hotel = hotel_name.replace(" ", "_")
+        db['hoteis'][new_hotel] = {"reservas": {}, "quartos": {}}
+        return 1, new_hotel
+
+def adicionar_quarto(num_quarto, hotel):
+    if num_quarto in db['hoteis'][hotel]['quartos']:
+        return 0, "Quarto ja existente"
+    else:
+        db['hoteis'][hotel]['quartos'][num_quarto] = "livre"
+        return 1, hotel
+
+def excluir_hotel(hotel, user_id):
+    if hotel in db['hoteis']:
+        all_reservas = get_all_reservas()
+        for reserva_id, reserva in all_reservas.items():
+            if reserva["hotel"] == hotel:
+                for usuario_id in db["usuarios"].keys():
+                    if reserva_id in db["usuarios"][usuario_id]["reservas"]:
+                        db["usuarios"][usuario_id]["reservas"].remove(reserva_id)
+                del db["hoteis"][hotel]["reservas"][reserva_id]
+    
+        return 1, db['usuarios'][user_id]
+    else:
+        return 0, "Erro ao excluir hotel"
+
+def excluir_quarto(hotel, quarto, user_id):
+    if quarto in db["hoteis"][hotel]["quartos"]:
+            del db["hoteis"][hotel]["quartos"][quarto]
+
+            all_reservas = get_all_reservas()
+            for reserva_id, reserva in all_reservas.items():
+                if reserva['hotel'] == hotel and reserva['quarto'] == quarto:
+                    for usuario_id in db["usuarios"].keys():
+                        if reserva_id in db["usuarios"][usuario_id]["reservas"]:
+                            db["usuarios"][usuario_id]["reservas"].remove(reserva_id)
+                    del db["hoteis"][hotel]["reservas"][reserva_id]
+            
+            return 1, db['usuarios'][user_id]
+    else:
+        return 0, "Quarto inexistente"
+
 def main():
     servidor = SimpleXMLRPCServer(("localhost", 8000))
 
@@ -107,6 +197,12 @@ def main():
     servidor.register_function(reserva, "reserva")
     servidor.register_function(get_reservas, "get_reservas")
     servidor.register_function(excluir_reserva, "excluir_reserva")
+    servidor.register_function(get_all_hoteis, "get_all_hoteis")
+    servidor.register_function(get_quartos, "get_quartos")
+    servidor.register_function(adicionar_hotel, "adicionar_hotel")
+    servidor.register_function(adicionar_quarto, "adicionar_quarto")
+    servidor.register_function(excluir_quarto, "excluir_quarto")
+    servidor.register_function(excluir_hotel, "excluir_hotel")
     
     print("Servidor iniciado...")
     servidor.serve_forever()
